9a5c427ee69b07144bb7d54cfcb9132a6bcf4262
Sina Akbari <sina.akbari1996@gmail.com>
2024-10-24 22:42:41 -0700

    feat: add deck inherent implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+#[derive(Debug)]
+struct Deck {
+    cards: Vec<String>,
+}
+
+impl Deck {
+    fn new() -> Self {
+        let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
+        let values = ["Ace", "Two", "Three"];
+        let mut cards = vec![];
+
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card);
+            }
+        }
+
+        Deck { cards }
+    }
+
+    fn shuffle(&self) {}
+}
+
 fn main() {
-    println!("Hello, world!");
+    let deck = Deck::new();
+
+    deck.shuffle();
+
+    println!("Here's the deck: {:#?}", deck);
 }
