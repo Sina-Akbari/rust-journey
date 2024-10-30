2c9cbacc4e430a5c13f288f9b314760d806e9b9b
Sina Akbari <sina.akbari1996@gmail.com>
2024-10-29 21:21:23 -0700

    feat: read a file

--- logs/src/main.rs
+++ logs/src/main.rs
@@ -1,38 +1,12 @@
 use std::fs;
-use std::io::Error;
 
 fn main() {
-    // let text = fs::read_to_string("logs.txt");
-
-    // println!("{:#?}", text);
-
-    match divide(5.0, 0.0) {
-        Ok(value) => {
-            println!("{}", value);
+    match fs::read_to_string("logs.txt") {
+        Ok(text) => {
+            println!("{}", text.len());
         }
         Err(error) => {
-            println!("{}", error);
+            println!("Failed to read file: {}", error);
         }
-    }
-
-    match validate_email(String::from("asdf@asdf.com")) {
-        Ok(..) => println!("email is valid"),
-        Err(error) => println!("{}", error),
-    }
-}
-
-fn validate_email(email: String) -> Result<(), Error> {
-    if email.contains("@") {
-        Ok(())
-    } else {
-        Err(Error::other("emails must have an @"))
-    }
-}
-
-fn divide(a: f64, b: f64) -> Result<f64, Error> {
-    if b == 0.0 {
-        Err(Error::other("can't devide by 0!"))
-    } else {
-        Ok(a / b)
     }
 }
