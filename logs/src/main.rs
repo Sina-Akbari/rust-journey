4775f1bc5fc29aa5ed4cf66bd56f07d8284bd644
Sina Akbari <sina.akbari1996@gmail.com>
2024-10-29 21:18:56 -0700

    feat: do some validation

--- logs/src/main.rs
+++ logs/src/main.rs
@@ -1,3 +1,38 @@
+use std::fs;
+use std::io::Error;
+
 fn main() {
-    println!("Hello, world!");
+    // let text = fs::read_to_string("logs.txt");
+
+    // println!("{:#?}", text);
+
+    match divide(5.0, 0.0) {
+        Ok(value) => {
+            println!("{}", value);
+        }
+        Err(error) => {
+            println!("{}", error);
+        }
+    }
+
+    match validate_email(String::from("asdf@asdf.com")) {
+        Ok(..) => println!("email is valid"),
+        Err(error) => println!("{}", error),
+    }
+}
+
+fn validate_email(email: String) -> Result<(), Error> {
+    if email.contains("@") {
+        Ok(())
+    } else {
+        Err(Error::other("emails must have an @"))
+    }
+}
+
+fn divide(a: f64, b: f64) -> Result<f64, Error> {
+    if b == 0.0 {
+        Err(Error::other("can't devide by 0!"))
+    } else {
+        Ok(a / b)
+    }
 }
