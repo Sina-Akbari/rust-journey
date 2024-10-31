9919fd70feb86527ef7ece42b3e024449e4078c6
Sina Akbari <sina.akbari1996@gmail.com>
2024-10-30 21:32:34 -0700

    feat: store error logs into a file

--- logs/src/main.rs
+++ logs/src/main.rs
@@ -1,9 +1,28 @@
 use std::fs;
 
+fn extract_errors(text: &str) -> Vec<String> {
+    let split_text = text.split("\n");
+
+    let mut results = vec![];
+
+    for line in split_text {
+        if line.starts_with("ERROR") {
+            results.push(line.to_string());
+        }
+    }
+
+    results
+}
+
 fn main() {
     match fs::read_to_string("logs.txt") {
         Ok(text) => {
-            println!("{}", text.len());
+            let error_logs = extract_errors(text.as_str());
+
+            match fs::write("errors.txt", error_logs.join("\n")) {
+                Ok(..) => println!("Errors extracted successfully!"),
+                Err(error) => println!("{}", error),
+            }
         }
         Err(error) => {
             println!("Failed to read file: {}", error);
