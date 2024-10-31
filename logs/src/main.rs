48cb8bf10b3052a7043e02b2881b37c8ef4d24fe
Sina Akbari <sina.akbari1996@gmail.com>
2024-10-30 21:33:28 -0700

    feat: panic if there's an error in reading/writing to a file

--- logs/src/main.rs
+++ logs/src/main.rs
@@ -15,17 +15,22 @@ fn extract_errors(text: &str) -> Vec<String> {
 }
 
 fn main() {
-    match fs::read_to_string("logs.txt") {
-        Ok(text) => {
-            let error_logs = extract_errors(text.as_str());
+    let text = fs::read_to_string("logs.txt").expect("Failed to read logs.txt");
+    let errors = extract_errors(text.as_str());
 
-            match fs::write("errors.txt", error_logs.join("\n")) {
-                Ok(..) => println!("Errors extracted successfully!"),
-                Err(error) => println!("{}", error),
-            }
-        }
-        Err(error) => {
-            println!("Failed to read file: {}", error);
-        }
-    }
+    fs::write("errors.txt", errors.join("\n")).expect("Failed to write to errors.txt");
+
+    // match fs::read_to_string("logs.txt") {
+    //     Ok(text) => {
+    //         let error_logs = extract_errors(text.as_str());
+
+    //         match fs::write("errors.txt", error_logs.join("\n")) {
+    //             Ok(..) => println!("Errors extracted successfully!"),
+    //             Err(error) => println!("{}", error),
+    //         }
+    //     }
+    //     Err(error) => {
+    //         println!("Failed to read file: {}", error);
+    //     }
+    // }
 }
