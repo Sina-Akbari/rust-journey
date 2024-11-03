bbbfd4db6574503e5315603610f6b9c14081fbc6
Sina Akbari <sina.akbari1996@gmail.com>
2024-11-02 23:44:31 -0700

    feat(iter): add to_uppercase function

--- iter/src/main.rs
+++ iter/src/main.rs
@@ -13,14 +13,19 @@ fn shorten_strings(elements: &mut [String]) {
     elements.iter_mut().for_each(|el| el.truncate(1));
 }
 
+fn to_uppercase(elements: &[String]) -> Vec<String> {
+    elements.iter().map(|el| el.to_uppercase()).collect()
+}
+
 fn main() {
-    let mut colors = vec![
+    let colors = vec![
         String::from("red"),
         String::from("green"),
         String::from("blue"),
     ];
 
-    shorten_strings(&mut colors);
-
-    print_elements(&mut colors);
+    let uppercased = to_uppercase(&colors);
+    println!("{:#?}", uppercased);
+    // shorten_strings(&mut colors);
+    // print_elements(&mut colors);
 }
