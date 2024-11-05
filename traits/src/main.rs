6a62d05177dce8882f0973d38bf805143eca1460
Sina Akbari <sina.akbari1996@gmail.com>
2024-11-04 21:46:06 -0800

    feat(traits): use trait to define a generic function

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -3,14 +3,22 @@ mod container;
 mod stack;
 
 use basket::Basket;
+use container::Container;
 use stack::Stack;
 
+fn add_string<T: Container<String>>(container: &mut T, value: String) {
+    container.put(value);
+}
+
 fn main() {
-    let b1 = Basket::new(String::from("Hi there!"));
+    let mut b1 = Basket::new(String::from("Hi there!"));
     let b2 = Basket::new(4);
     let b3 = Basket::new(false);
 
-    let s1 = Stack::new(vec![String::from("Hi there!")]);
+    let mut s1 = Stack::new(vec![String::from("Hi there!")]);
     let s2 = Stack::new(vec![4]);
     let s3 = Stack::new(vec![false]);
+
+    add_string(&mut b1, String::from("Some String"));
+    add_string(&mut s1, String::from("Some String"));
 }
