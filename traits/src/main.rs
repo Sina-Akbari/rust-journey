eec534d795b6e268b42b2d71e58968ee7767b672
Sina Akbari <sina.akbari1996@gmail.com>
2024-11-04 21:26:38 -0800

    feat(traits): implement basket data structure

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,3 +1,9 @@
+mod basket;
+
+use basket::Basket;
+
 fn main() {
-    println!("Hello, world!");
+    let b1 = Basket::new(String::from("Hi there!"));
+    let b2 = Basket::new(4);
+    let b3 = Basket::new(false);
 }
