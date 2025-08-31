9224c0a50ac081a4fd39f84bd469f802cf89f930
Sina <sina.akbari1996@gmail.com>
2025-08-31 12:32:48 -0700

    fix: double ended iterator by using two inner pointers

--- flatten/src/lib.rs
+++ flatten/src/lib.rs
@@ -1,3 +1,21 @@
+pub trait IteratorExt: Iterator + Sized {
+    fn custom_flatten(self) -> Flatten<Self>
+    where
+        Self::Item: IntoIterator;
+}
+
+impl<T> IteratorExt for T
+where
+    T: Iterator,
+{
+    fn custom_flatten(self) -> Flatten<Self>
+    where
+        Self::Item: IntoIterator,
+    {
+        flatten(self)
+    }
+}
+
 pub fn flatten<I>(iter: I) -> Flatten<I::IntoIter>
 where
     I: IntoIterator,
@@ -12,7 +30,8 @@ where
     O::Item: IntoIterator,
 {
     outer: O,
-    inner: Option<<O::Item as IntoIterator>::IntoIter>,
+    front_iter: Option<<O::Item as IntoIterator>::IntoIter>,
+    back_iter: Option<<O::Item as IntoIterator>::IntoIter>,
 }
 
 impl<O> Flatten<O>
@@ -23,7 +42,8 @@ where
     fn new(iter: O) -> Self {
         Self {
             outer: iter,
-            inner: None,
+            front_iter: None,
+            back_iter: None,
         }
     }
 }
@@ -37,36 +57,42 @@ where
 
     fn next(&mut self) -> Option<Self::Item> {
         loop {
-            if let Some(ref mut inner_iter) = self.inner {
-                if let Some(i) = inner_iter.next() {
+            if let Some(ref mut front_iter) = self.front_iter {
+                if let Some(i) = front_iter.next() {
                     return Some(i);
                 }
-                self.inner = None
+                self.front_iter = None
             }
 
-            let next_inner_iter = self.outer.next()?.into_iter();
-            self.inner = Some(next_inner_iter);
+            if let Some(next_front) = self.outer.next() {
+                self.front_iter = Some(next_front.into_iter());
+            } else {
+                return self.back_iter.as_mut()?.next();
+            }
         }
     }
 }
 
 impl<O> DoubleEndedIterator for Flatten<O>
 where
-    O: Iterator + DoubleEndedIterator,
+    O: DoubleEndedIterator,
     O::Item: IntoIterator,
     <O::Item as IntoIterator>::IntoIter: DoubleEndedIterator,
 {
     fn next_back(&mut self) -> Option<Self::Item> {
         loop {
-            if let Some(ref mut inner_iter) = self.inner {
-                if let Some(i) = inner_iter.next_back() {
+            if let Some(ref mut back_iter) = self.back_iter {
+                if let Some(i) = back_iter.next_back() {
                     return Some(i);
                 }
-                self.inner = None
+                self.back_iter = None
             }
 
-            let next_inner_iter = self.outer.next_back()?.into_iter();
-            self.inner = Some(next_inner_iter);
+            if let Some(next_back) = self.outer.next_back() {
+                self.back_iter = Some(next_back.into_iter());
+            } else {
+                return self.front_iter.as_mut()?.next();
+            }
         }
     }
 }
@@ -113,5 +139,45 @@ mod tests {
                 .collect::<Vec<_>>(),
             vec!["c", "b", "a"]
         )
+    }
+
+    #[test]
+    fn both_ends() {
+        let mut iter = flatten(vec![vec!["a1", "a2", "a3"], vec!["b1", "b2", "b3"]]);
+
+        assert_eq!(iter.next(), Some("a1"));
+        assert_eq!(iter.next_back(), Some("b3"));
+        assert_eq!(iter.next(), Some("a2"));
+        assert_eq!(iter.next_back(), Some("b2"));
+        assert_eq!(iter.next(), Some("a3"));
+        assert_eq!(iter.next_back(), Some("b1"));
+        assert_eq!(iter.next(), None);
+        assert_eq!(iter.next_back(), None);
+    }
+
+    #[test]
+    fn infinite() {
+        let mut iter = flatten((0..).map(|i| 0..i));
+        // 0 => 0..0 => empty
+        // 1 => 0..1 => 0
+        // 2 => 0..2 => 0..1
+        // 3 => 0..3 => 0..2
+
+        assert_eq!(iter.next(), Some(0));
+        assert_eq!(iter.next(), Some(0));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), Some(0));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), Some(2));
+    }
+
+    #[test]
+    fn deep() {
+        assert_eq!(flatten(flatten(vec![vec![vec![0, 1]]])).count(), 2);
+    }
+
+    #[test]
+    fn ext() {
+        assert_eq!(vec![vec![0, 1]].into_iter().custom_flatten().count(), 2);
     }
 }
